74b937a1a9f9101fc2ab35a1e882dc05787c1b0f
Austin-Sands <austin_sands@comcast.net>
2024-01-26 19:12:17 -0500

    last commit mistakenly pushed in commented form for testing purposes. Uncommented to functional state

--- die_visual _variable_dice.py
+++ die_visual _variable_dice.py
@@ -49,32 +49,30 @@ while True:
     else:
         print(f"{new_dice_sides} is not a valid numeric value.")
 
-print(get_max_roll())
+# make some rolls and store results in list
+results = []
+results = [roll_dice() for roll_num in range(50_000)]
 
-# # make some rolls and store results in list
-# results = []
-# results = [roll_dice() for roll_num in range(50_000)]
+# get frequency of results
+frequencies = []
+max_roll = get_max_roll()
+poss_results = range(len(dice), max_roll+1)
+frequencies = [results.count(value) for value in poss_results]
 
-# # get frequency of results
-# frequencies = []
-# max_roll = get_max_roll()
-# poss_results = range(len(dice), max_roll+1)
-# frequencies = [results.count(value) for value in poss_results]
+# visualize the results
+dice_string = get_dice_string()
+title = f"Results of Rolling a {dice_string} 50,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 
-# # visualize the results
-# dice_string = get_dice_string()
-# title = f"Results of Rolling a {dice_string} 50,000 Times"
-# labels = {'x': 'Result', 'y': 'Frequency of Result'}
-# fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+# further customization
+if len(poss_results) > 20:
+    dtick = int(len(poss_results) * .05)
+else:
+    dtick = 1
 
-# # further customization
-# if len(poss_results) > 20:
-#     dtick = int(len(poss_results) * .05)
-# else:
-#     dtick = 1
+fig.update_layout(xaxis_dtick=dtick)
 
-# fig.update_layout(xaxis_dtick=dtick)
+fig.show()
 
-# fig.show()
-
-# print(frequencies)
+print(frequencies)
